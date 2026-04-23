a0342df0d2e27f9dc5114f024d493afd55d5fcaa
CyberHive <cyberhives@protonmail.com>
2026-04-23 13:03:55 +0200

    fix: remove duplicate code in MainActivity.kt that broke compilation

--- android/app/src/main/java/com/cyberclawmobile/MainActivity.kt
+++ android/app/src/main/java/com/cyberclawmobile/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.speech.RecognizerIntent
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.bridge.ActivityEventListener
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -51,34 +50,6 @@ class MainActivity : ReactActivity() {
 
   // Called by WakeWordModule to launch the speech intent
   fun startSpeechRecognition(promise: Promise) {
-    speechPromise = promise
-    val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-      putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-      putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1)
-      putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...")
-    }
-    try {
-      startActivityForResult(intent, SPEECH_REQUEST_CODE)
-    } catch (e: Exception) {
-      speechPromise?.reject("error", e.message)
-      speechPromise = null
-    }
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    if (requestCode == SPEECH_REQUEST_CODE) {
-      if (resultCode == Activity.RESULT_OK) {
-        val results = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-        val text = results?.firstOrNull() ?: ""
-        speechPromise?.resolve(text)
-      } else {
-        speechPromise?.reject("cancelled", "Speech cancelled")
-      }
-      speechPromise = null
-    }
-  }
-}
     speechPromise = promise
     val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
       putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
